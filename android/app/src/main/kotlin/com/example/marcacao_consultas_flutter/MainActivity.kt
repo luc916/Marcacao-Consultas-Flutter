10a4cb6d41eb56d222e9f75c22e4a9e56935a79d
lucas kenzo nishiwaki <luc@MacBook-Air-de-lucas.local>
2026-08-24 20:20:42 -0300

    Aula 10/08 - Inicia projeto Flutter e organiza arquitetura base do Sistema de Consultas

---
+++ android/app/src/main/kotlin/com/example/marcacao_consultas_flutter/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.marcacao_consultas_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+import 'src/screens/screens.dart';
+import 'src/styles/app_colors.dart';
+
+void main() {
+  runApp(const MarcacaoConsultasApp());
+}
+
+class MarcacaoConsultasApp extends StatelessWidget {
+  const MarcacaoConsultasApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Sistema de Consultas',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: AppColors.primaria),
+        useMaterial3: true,
+      ),
+      home: const HomeScreen(),
+    );
+  }
+}
